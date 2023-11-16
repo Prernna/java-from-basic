4ee3cccdd90b666f05ee474566335655b4162628
Prernna <126183593+Prernna@users.noreply.github.com>
2023-11-17 01:35:05 +0530

    Add files via upload
    simple array defining through loops.

---
+++ array2.java
@@ -0,0 +1,12 @@
+public class array2 {
+    public static void main(String [] args){
+
+        double[] dsw={1234.0000,3456,3443534};
+        for(double i=0;i<3;i++){
+            System.out.println(dsw[3]);
+        }
+        System.out.println(dsw[0]);
+        
+    }
+    
+}
