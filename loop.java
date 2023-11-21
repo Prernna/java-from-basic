7c13e17de5b99d0f4e93193b1867781b274f5baa
Prernna <126183593+Prernna@users.noreply.github.com>
2023-11-22 00:03:01 +0530

    Create loop.java
    In this code we have used for loop to print " * " in different ways vertically and horizontally .

---
+++ loop.java
@@ -0,0 +1,11 @@
+public class loop{
+    public static void main(String [] prerna){
+        //loop
+        for(int i=0;i<3;i++){
+            System.out.println("*");
+            System.out.print("*");
+            System.out.print("Nosetyper");
+
+        }
+    }
+}
