49ce90030825ef7cb91947c7b7a4660fe5a18516
Prernna <126183593+Prernna@users.noreply.github.com>
2023-11-23 01:20:31 +0530

    Create logicalopt.java
    In this code, we will learn about logical and,or and not operators using if-else statements.

---
+++ logicalopt.java
@@ -0,0 +1,38 @@
+public class vid17 {
+   public static void main(String[] args) {
+   System.out.println("For Logical AND opetar");
+   boolean a = true;
+   boolean b = false;
+
+   if(a && b){
+    System.out.println("Yes");
+   }
+   else{
+    System.out.println("No");
+   }
+   System.out.println(" ");
+   System.out.println("For Logical OR operator");
+   if(a || b){
+    System.out.println(" will print true if one of the condition is true");
+    System.out.println("yes, Condition is fullfilled");
+   }
+   else{
+    System.out.println("Print false if both the condition is false");
+   }
+   System.out.println(" ");
+   System.out.println("For Logical NOT operator");
+   if(a!=b){
+    System.out.println("condition is false");
+   }
+   else{
+    System.out.println("Given condition is true");
+   }
+System.out.println(" ");
+System.out.println("NOT (a) is");
+System.out.println(!a);
+System.out.println("NOT(b) is");
+System.out.println(!b);
+
+
+}
+   }
