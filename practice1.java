a699a8a55b71f95f5669e5b74ec76e2975da278d
Prernna <126183593+Prernna@users.noreply.github.com>
2023-11-25 02:29:12 +0530

    Create practice1.java
    In this code we have a basic ques related to if-else statement that we have to guess the output.

---
+++ practice1.java
@@ -0,0 +1,9 @@
+// 1. What will be the output of this program: int a =10; if(a=11) system.ot.println("I am 11"); else System.out.println("I am not 11")
+
+int a=10;
+if(a==11){
+    System.out.println("I am 11");
+}
+else{
+    System.out.println("I am not 11");
+}
