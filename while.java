5efabf2579aea2e44bac19153c3ce7d39f6990c6
Prernna <126183593+Prernna@users.noreply.github.com>
2023-11-27 00:22:20 +0530

    Create while.java
    In this code we will learn about while loops and a basic function to print from 100 to 200 in a simpler way.

---
+++ while.java
@@ -0,0 +1,17 @@
+public class vid21 {
+    public static void main(String[] args) {
+    
+        int i=0;
+        while (i<=3) {
+            System.out.println(i);
+            i++;
+        }
+ int j=100;
+ while(j<=200)
+{
+    System.out.println(j);
+    j++;
+}
+    }
+
+}
