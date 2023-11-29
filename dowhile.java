13bf3340ba3863e3fdc52d4f3802c108b7b3b2c4
Prernna <126183593+Prernna@users.noreply.github.com>
2023-11-29 14:37:12 +0530

    Create dowhile.java
    in this program, we will learn about do-while loops and their functions by putting boolean conditions inside.

---
+++ dowhile.java
@@ -0,0 +1,32 @@
+public class vid22{
+public static void main(String []args){
+ 
+int a=10;
+for(int i=0;i<a.length;i++){
+System.out.println(a);
+}
+
+int b=8;
+while(b<16){
+System.out.println(b);
+}
+
+// the difference btw while and do while loop is that while runs with a condition but in do while . Do functions without checking the condition so it will run but it will only execute if the while condition is true execution is very important.It is guarenteed to execute once.
+
+int c=15;
+do{
+System.out.println(c);
+c++;
+}
+while(c<30);
+
+// Write a program to print first n natural numbers using do while loop
+
+int d =0;
+do{
+System.out.println(d);
+d++;
+}
+while(d<100);
+
+}
