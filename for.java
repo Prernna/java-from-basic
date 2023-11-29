6b88bedb8d0031644190cf38a232758070d036aa
Prernna <126183593+Prernna@users.noreply.github.com>
2023-11-30 03:13:11 +0530

    Create for.java
    for loop

---
+++ for.java
@@ -0,0 +1,43 @@
+public class vid23{
+public static void main(String []args){
+
+for(int i=0;i<+10;i++){
+System.out.println(i);
+}
+
+// a for loop is usually used to execute a peice of code for specific number of times.
+
+// print 5 no. of odd
+// 2i=2,4,6,8,10
+//2i+1=3,5,7,9
+
+int n =5;
+for(int i=0;i<n;i++)
+{
+System.out.println(2*i+1);
+}
+
+//Decrementing for loop
+
+for(int i=7;i>0;i--)
+{
+System.out.println(i);
+}
+
+//or it can also be written as
+
+for(int i=7;i!=0;i--)
+{
+System.out.println(i);
+}
+
+//write a program to print first n natural numbers in reverse order , n can be any number
+
+for(int i = 200;i<0;i--){
+System.out.println(i);
+}
+
+
+
+}
+}
