f192affba872fd3f80fa0cb0dd35ea42ceed9961
Prernna <126183593+Prernna@users.noreply.github.com>
2023-12-01 20:20:16 +0530

    Create forloop.java
    Basic program to run char datatype to print the number of alphabets in English .

---
+++ forloop.java
@@ -0,0 +1,10 @@
+public class Alphabets
+{
+	public static void main(String[] args) {
+	    char c ;
+	    System.out.println("Here are the alphabets from a to z. ");
+for ( c = 'a'; c <= 'z'; c++) {
+    System.out.print(c + " ");
+}
+	}
+}
