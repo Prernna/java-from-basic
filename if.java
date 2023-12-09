d34e844b934038d26b7adfee42756066ff9ef093
Prernna <126183593+Prernna@users.noreply.github.com>
2023-12-10 01:06:50 +0530

    Create if.java

---
+++ if.java
@@ -0,0 +1,11 @@
+int num = 2; // Example variable
+
+if (num == 1) {
+    System.out.println("Number is 1");
+} else if (num == 2) {
+    System.out.println("Number is 2");
+} else if (num == 3) {
+    System.out.println("Number is 3");
+} else {
+    System.out.println("Number is not 1, 2, or 3");
+}
