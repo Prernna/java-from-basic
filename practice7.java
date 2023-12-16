0caaa9124791b77f90306baf11388577db304a0a
Prernna <126183593+Prernna@users.noreply.github.com>
2023-12-16 17:17:34 +0530

    Create practice7.java
    Write a program to make decrementing and incrementing '*" values.

---
+++ practice7.java
@@ -0,0 +1,34 @@
+public class vid25 {
+    public static void main (String [] args){
+
+// practice questions
+//1. Write a program to print the following pattern 
+// ****
+// ***
+// **
+// *
+
+int a = 4;
+for(int i=a;i>0;i--){
+    for(int j=0;j<i;j++){
+    System.out.print("*");
+
+    }
+    System.out.print("\n");
+}
+
+
+for(int k=4;k<12;k++){
+    for(int l=0;l<k;l++){
+    System.out.print("*");
+
+    }
+    System.out.print("\n");
+}
+
+
+
+
+    }
+
+}
