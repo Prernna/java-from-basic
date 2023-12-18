3db0a9e371cc7820bed3cb9fa4225fd397c6da38
Prernna <126183593+Prernna@users.noreply.github.com>
2023-12-19 00:47:00 +0530

    Create problem9.java

---
+++ problem9.java
@@ -0,0 +1,13 @@
+//a do while loop is executed: 1> at least once , 2>atleast twice, 3> atleast thrice 
+        
+        //repeat 2 using for loop
+        
+        for(int i=0;i<=5;i++){
+            System.out.println(2);
+        }
+int n=2;
+        int sum=0;
+        for(int i=1;i<=10;i++){
+            sum += n*i;
+        }
+        System.out.println(sum);
