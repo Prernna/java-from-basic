d065d05fa7db1b682d8403b27cadcf75124c28b0
Prernna <126183593+Prernna@users.noreply.github.com>
2023-12-30 10:18:13 +0530

    Create OOPsintro.java
    Basic introduction about OOPs and their needs in the real world.

---
+++ OOPsintro.java
@@ -0,0 +1,28 @@
+public class vid36introOOPs {
+    
+    public static void main(String[] args) {
+        
+/*What is object oriented programming?
+solving a problem by creating objrcts is one of the most popular approaches in programming .this is called OOPs 
+DRY -> Do not repeat yourself (focus on code reusability)
+
+OBJECT -> object is an instantiation  of a class.When a class is defined , a template(info) is defined.Memory is allocated only after object instantiation.
+
+How to model a problem in OOPs , identify the following:
+noun -> class -> Employee
+Adjective -> Attributes ->name age,salary
+Verb -> Methods -> get Salary() , increment()
+
+OOPs terminology
+1.Abstraction : hiding internal details(shows only essential info!) ex: Android app 
+2.Encapsulation : The act of putting various components together (in a  capsule). it simply means that the sensitive data can be hidden from the users.
+3.Inheritance : The act of deriving new things from existing things. //implements DRY
+4.Polymorphism->one entity more focus
+
+
+
+
+*/
+
+    }
+}
